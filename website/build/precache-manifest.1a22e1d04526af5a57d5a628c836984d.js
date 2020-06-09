66ccbf3a6e94283b75ef03c9d27bb34a625282cd
RenanErnest <renan.erns@gmail.com>
2020-06-08 23:46:16 -0300

    Grouping the rest api server with the website

---
+++ website/build/precache-manifest.1a22e1d04526af5a57d5a628c836984d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56c0d3908e4180ee8b7d46510b4cec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bafa89b02bff19d9959",
+    "url": "/static/css/main.802688e4.chunk.css"
+  },
+  {
+    "revision": "e242926b3a12125a91cb",
+    "url": "/static/js/2.1c13773c.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/static/js/2.1c13773c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bafa89b02bff19d9959",
+    "url": "/static/js/main.4dfa2d48.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
