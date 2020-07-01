9108be3a222bf59dec87248a6c7f9b92e27cc488
RenanErnest <renan.erns@gmail.com>
2020-07-01 01:48:05 -0300

    Some fixes

--- website/functions/index.js
+++ website/functions/index.js
@@ -78,87 +78,19 @@ soap = require('soap'),
 express = require('express'),
 lastReqAddress;
 var server = express();
-service = {
-    StockQuoteService: {
-        StockQuotePort: {
-            GetLastTradePrice: function (args, cb, soapHeader) {
-                if (soapHeader)
-                    return {
-                        price: soapHeader.SomeToken
-                    };
-                if (args.tickerSymbol === 'trigger error') {
-                    throw new Error('triggered server error');
-                } else if (args.tickerSymbol === 'Async') {
-                    return cb({
-                        price: 19.56
-                    });
-                } else if (args.tickerSymbol === 'SOAP Fault v1.2') {
-                    throw {
-                        Fault: {
-                            Code: {
-                                Value: "soap:Sender",
-                                Subcode: {
-                                    value: "rpc:BadArguments"
-                                }
-                            },
-                            Reason: {
-                                Text: "Processing Error"
-                            }
-                        }
-                    };
-                } else if (args.tickerSymbol === 'SOAP Fault v1.1') {
-                    throw {
-                        Fault: {
-                            faultcode: "soap:Client.BadArguments",
-                            faultstring: "Error while processing arguments"
-                        }
-                    };
-                } else {
-                    return {
-                        price: 19.56
-                    };
-                }
-            },
-
-            SetTradePrice: function (args, cb, soapHeader) {},
-
-            IsValidPrice: function (args, cb, soapHeader, req) {
-                lastReqAddress = req.connection.remoteAddress;
-
-                var validationError = {
-                    Fault: {
-                        Code: {
-                            Value: "soap:Sender",
-                            Subcode: {
-                                value: "rpc:BadArguments"
-                            }
-                        },
-                        Reason: {
-                            Text: "Processing Error"
-                        },
-                        statusCode: 500
-                    }
-                };
-
-                var isValidPrice = function () {
-                    var price = args.price;
-                    if (isNaN(price) || (price === ' ')) {
-                        return cb(validationError);
-                    }
-
-                    price = parseInt(price, 10);
-                    var validPrice = (price > 0 && price < Math.pow(10, 5));
-                    return cb(null, {
-                        valid: validPrice
-                    });
-                };
-
-                setTimeout(isValidPrice, 10);
-            }
-        }
-    }
+var service = {
+  PurchaseOrderService: {
+      PurchaseOrderServiceSoapHttpPort: {
+          getPurchaseOrder: function(args) {
+              console.log(args);
+              return {
+                  name: args.name
+              };
+          }
+      }
+   }
 };
-var wsdl = fs.readFileSync(__dirname + '/wsdl/stockquote.wsdl', 'utf-8').toString();
+var wsdl = fs.readFileSync(__dirname + '/wsdl/PurchaseOrderServiceV2.wsdl', 'utf-8').toString();
 server = express();
 
 soapServer = soap.listen(server, '/', service, wsdl)
