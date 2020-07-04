affbaa2f74aac5f2829a40ce6d2fd8a8215944f6
RenanErnest <renan.erns@gmail.com>
2020-07-04 18:26:02 -0300

    Cart on deadend-point

--- website/src/Components/DeadEnd.js
+++ website/src/Components/DeadEnd.js
@@ -9,6 +9,7 @@ class DeadEnd extends Component{
                     <h1>Oops!</h1>
                     <p>Parece que você chegou no fim do caminho.</p>
                     <p>Ainda não vendemos passagens aéreas e essas coisas, mas você pode nos contar se gostou do que viu até aqui.</p>
+                    <p><a className="btn btn-primary btn-outline btn-lg" href="/cart">Confira seu carrinho <i className="icon-arrow-right22"></i></a></p>
                     <p><a className="btn btn-primary btn-outline btn-lg" href="/contact">Fale com a gente <i className="icon-arrow-right22"></i></a></p>
                     <p><a className="btn btn-primary btn-outline btn-lg" href="/"><i className="icon-arrow-left22"></i> Volte para o começo</a></p>
                 </div>
